314ad3172e18ec3e146e1c0f22c5657464a1220c
Nakakandebr <bridgetnakakandea@gmail.com>
2023-05-20 17:33:10 +0300

    commit

--- fruit.py
+++ fruit.py
@@ -6,13 +6,13 @@ class Fruit:
         self.color = color
         self.price = price
         
-    def wash(self):
+    def ripen(self):
         return f" Bridget is washing the {self.color} {self.name}  which is a {self.category} now ."
     
-    def peel(self):
+    def rot(self):
         return f" They are Peeling the {self.color} {self.name} using a knife."
     
-    def eat(self):
+    def grow(self):
         return f" Children are eating the {self.color} {self.name} which cost  {self.price} dollars per pound."
     
     
